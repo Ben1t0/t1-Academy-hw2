f498647334348b8119ccdb0379984aa27e9413b2
Georgiy Rusakov <gosha1510@yandex.ru>
2024-08-13 20:21:51 +0300

    init

---
+++ src/test/java/ru/t1academy/java/hw2/Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1academy.java.hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
